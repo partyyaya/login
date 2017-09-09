e68d8a793327809256722723f920f3bcc5900ed3
user <user@user-PC>
2017-09-09 09:06:05 +0800

    2017

--- Log/src/tw/member/login/changeData.java
+++ Log/src/tw/member/login/changeData.java
@@ -54,6 +54,7 @@ public class changeData extends HttpServlet {
 			}catch (Exception e){
 				System.out.println(e);
 			}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
